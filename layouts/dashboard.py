df4c19ca2d3ea08ff852bbbbc8786baa25914b4b
brunopressi <brunopressi2012@gmail.com>
2026-04-20 09:31:42 -0300

    update graphs config

--- layouts/dashboard.py
+++ layouts/dashboard.py
@@ -10,21 +10,25 @@ chart_container_style = {
     'boxShadow': '0 4px 6px rgba(0, 0, 0, 0.05)'
 }
 
+config_graphs = {
+   'staticPlot': True
+}
+
 def create_dashboard():
     return html.Div([
         # Linha 1: KPI em destaque e os dois gráficos principais
         create_kpi_total_acidentes(),
-        html.Div(dcc.Graph(id='ACIDENTES_GRAPH'), style=chart_container_style),
-        html.Div(dcc.Graph(id='ACIDENTES_REGIAO_GRAPH'), style=chart_container_style),
+        html.Div(dcc.Graph(id='ACIDENTES_GRAPH', config=config_graphs), style=chart_container_style),
+        html.Div(dcc.Graph(id='ACIDENTES_REGIAO_GRAPH', config=config_graphs), style=chart_container_style),
 
         # Linha 2: Gráficos de detalhamento
-        html.Div(dcc.Graph(id='ACIDENTES_TURNO_GRAPH'), style=chart_container_style),
-        html.Div(dcc.Graph(id='ACIDENTES_TIPO_GRAPH'), style=chart_container_style),
-        html.Div(dcc.Graph(id='ACIDENTES_VEICULO_GRAPH'), style=chart_container_style),
+        html.Div(dcc.Graph(id='ACIDENTES_TURNO_GRAPH', config=config_graphs), style=chart_container_style),
+        html.Div(dcc.Graph(id='ACIDENTES_TIPO_GRAPH', config=config_graphs), style=chart_container_style),
+        html.Div(dcc.Graph(id='ACIDENTES_VEICULO_GRAPH', config=config_graphs), style=chart_container_style),
 
         # Linha 3: Gráfico de tendência ocupando toda a largura
         html.Div(
-            html.Div(dcc.Graph(id='ACIDENTES_MES_GRAPH'), style=chart_container_style), 
+            html.Div(dcc.Graph(id='ACIDENTES_MES_GRAPH', config=config_graphs), style=chart_container_style), 
             style={'gridColumn': '1 / span 3'} # Ocupa as 3 colunas da grade
         ),
     ], style={
